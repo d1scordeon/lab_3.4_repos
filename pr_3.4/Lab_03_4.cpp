a9965b8b531f4005b42537b85602214f65e1f870
Kateryna Bakhmat <kateryna.bakhmat.it.2021@lpnu.ua>
2021-09-30 09:09:13 +0300

    lab_3.4

---
+++ pr_3.4/Lab_03_4.cpp
@@ -0,0 +1,28 @@
+// Lab_03_4.cpp
+// Бахмат Катерина
+// Лабораторна робота № 3.4
+// Розгалуження, задане плоскою фігурою.
+// Варіант 1
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x; 
+	double y; 
+	double R;
+
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+
+	if (y = 0, (x* x) + (y * y) < R * R, y > x || y < x, (x * x) + (y * y) > R, y = 0)
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+
+
+
+	cin.get();
+	return 0;
+}
